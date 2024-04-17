92dbb87bb532a30f3169367c9f430f374cb2e353
Arthur Rosa <93738072+Arthur-Rosa@users.noreply.github.com>
2024-04-17 12:44:13 -0300

    :sparkles:

---
+++ estrutura.sql
@@ -0,0 +1,41 @@
+CREATE TABLE Alunos (
+    aluno_id INT PRIMARY KEY,
+    nome VARCHAR(255),
+    idade INT,
+    turma_id INT,
+    FOREIGN KEY (turma_id) REFERENCES Turmas(turma_id)
+);
+
+CREATE TABLE Turmas (
+    turma_id INT PRIMARY KEY,
+    descricao VARCHAR(255)
+);
+
+CREATE TABLE Professores (
+    professor_id INT PRIMARY KEY,
+    nome VARCHAR(255)
+);
+
+CREATE TABLE Turma_Professor (
+    turma_id INT,
+    professor_id INT,
+    PRIMARY KEY (turma_id, professor_id),
+    FOREIGN KEY (turma_id) REFERENCES Turmas(turma_id),
+    FOREIGN KEY (professor_id) REFERENCES Professores(professor_id)
+);
+
+CREATE TABLE Atividades (
+    atividade_id INT PRIMARY KEY,
+    descricao VARCHAR(255),
+    turma_id INT,
+    FOREIGN KEY (turma_id) REFERENCES Turmas(turma_id)
+);
+
+CREATE TABLE Notas (
+    aluno_id INT,
+    atividade_id INT,
+    nota DECIMAL(5, 2),
+    PRIMARY KEY (aluno_id, atividade_id),
+    FOREIGN KEY (aluno_id) REFERENCES Alunos(aluno_id),
+    FOREIGN KEY (atividade_id) REFERENCES Atividades(atividade_id)
+);
